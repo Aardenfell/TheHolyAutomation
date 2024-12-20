ad89256aeada1a8c5f2adffad5e7a69ddcd6be4d
Aardenfell <40066656+Aardenfell@users.noreply.github.com>
2024-12-20 09:01:58 -0600

    fix(triggers): enable case-insensitive matching for triggers
    - Updated the trigger handler to match user messages regardless of case.
    - Standardized the trigger execution by converting both message content and trigger names to lowercase.

--- events/triggerCreate.js
+++ events/triggerCreate.js
@@ -2,7 +2,7 @@
  * @file Main trigger handler file.
  * @author Aardenfell
  * @since 1.0.0
- * @version 1.0.0
+ * @version 2.7.1
  */
 const { Events } = require("discord.js");
 
@@ -16,23 +16,12 @@ module.exports = {
 	 */
 
 	async execute(message) {
-		/**
-		 * @description The Message Content of the received message seperated by spaces (' ') in an array, this excludes prefix and command/alias itself.
-		 */
-
-		const args = message.content.split(/ +/);
-
-		// Checks if the trigger author is a bot. Comment this line if you want to reply to bots as well.
-
+		// Ignore messages from bots
 		if (message.author.bot) return;
 
-		// Checking ALL triggers using every function and breaking out if a trigger was found.
+		const messageContent = message.content.toLowerCase(); // Convert the message content to lowercase
 
-		/**
-		 * Checks if the message has a trigger.
-		 * @type {Boolean}
-		 * */
-
+		// Check all triggers
 		let triggered = false;
 
 		message.client.triggers.every((trigger) => {
@@ -41,23 +30,18 @@ module.exports = {
 			trigger.name.every(async (name) => {
 				if (triggered) return false;
 
-				// If validated, it will try to execute the trigger.
-
-				if (message.content.includes(name)) {
+				// Check if the trigger name matches the message content (case-insensitive)
+				if (messageContent.includes(name.toLowerCase())) {
 					try {
-						trigger.execute(message, args);
+						trigger.execute(message);
 					} catch (error) {
-						// If triggereds fail, reply back!
-
 						console.error(error);
-
 						message.reply({
-							content: "there was an error trying to execute that trigger!",
+							content: "There was an error trying to execute that trigger!",
 						});
 					}
 
-					// Set the trigger to be true & return.
-
+					// Mark as triggered
 					triggered = true;
 					return false;
 				}
